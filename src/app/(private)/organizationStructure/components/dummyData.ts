8e16807a67baf0d11a8313d1ed043114f842ef04
Ponna Ramesh <32008426+PONNARAMESH@users.noreply.github.com>
2024-03-13 23:43:51 +0530

    Add files via upload

---
+++ src/app/(private)/organizationStructure/components/dummyData.ts
@@ -0,0 +1,170 @@
+export const organizationInfo = {
+    "caseId": "Test123",
+    "ownershipStructureList": [
+      {
+        "name": "Mashreq1",
+        "relationWithCompany": {
+          "name": "Shareholder Entity",
+          "value": "800"
+        },
+        "belongsTo": {
+          "name": "Abhilash M",
+          "value": "Abhilash M",
+          "level": 1
+        },
+        "percentage": "35",
+        "signatory": "yes",
+        "accountServing": [
+          {
+            "name": "Callback",
+            "value": "781"
+          }
+        ],
+        "id": 14
+      },
+      {
+        "name": "Mashreq2",
+        "relationWithCompany": {
+          "name": "Shareholder Entity",
+          "value": "800"
+        },
+        "belongsTo": {
+          "name": "Abhilash M",
+          "value": "Abhilash M",
+          "level": 1
+        },
+        "percentage": "20",
+        "signatory": "NA",
+        "accountServing": [
+          {
+            "name": "Callback",
+            "value": "781"
+          },
+          {
+            "name": "Transaction Advices",
+            "value": "783"
+          }
+        ],
+        "id": 15
+      },
+      {
+        "name": "Mashreq3",
+        "relationWithCompany": {
+          "name": "Shareholder Individual",
+          "value": "098581"
+        },
+        "belongsTo": {
+          "name": "Abhilash M",
+          "value": "Abhilash M",
+          "level": 1
+        },
+        "percentage": "20",
+        "signatory": "yes",
+        "accountServing": [
+          {
+            "name": "Callback",
+            "value": "781"
+          },
+          {
+            "name": "Transaction Advices",
+            "value": "783"
+          }
+        ],
+        "id": 16
+      },
+      {
+        "name": "Mashreq4",
+        "relationWithCompany": {
+          "name": "Shareholder Entity",
+          "value": "800"
+        },
+        "belongsTo": {
+          "name": "Mashreq1",
+          "value": "Mashreq1",
+          "level": 2
+        },
+        "percentage": "25",
+        "signatory": "yes",
+        "accountServing": [
+          {
+            "name": "Callback",
+            "value": "781"
+          },
+          {
+            "name": "Transaction Advices",
+            "value": "783"
+          }
+        ],
+        "id": 17
+      },
+      {
+        "name": "MashreqBankTwo",
+        "relationWithCompany": {
+          "name": "Shareholder Individual",
+          "value": "098581"
+        },
+        "belongsTo": {
+          "name": "Mashreq1",
+          "value": "Mashreq1",
+          "level": 2
+        },
+        "percentage": "20",
+        "signatory": null,
+        "accountServing": [
+          {
+            "name": "Transaction Advices",
+            "value": "783"
+          }
+        ],
+        "id": 23
+      },
+      {
+        "name": "MashreqBan",
+        "relationWithCompany": {
+          "name": "Key Individual (Non-Shareholder)",
+          "value": "802"
+        },
+        "belongsTo": {
+          "name": "Mashreq2",
+          "value": "Mashreq2",
+          "level": 2
+        },
+        "percentage": "20",
+        "signatory": null,
+        "accountServing": [],
+        "id": 25
+      },
+      {
+        "name": "new 1",
+        "relationWithCompany": {
+          "name": "Key Individual (Non-Shareholder)",
+          "value": "802"
+        },
+        "belongsTo": {
+          "name": "Mashreq2",
+          "value": "Mashreq2",
+          "level": 2
+        },
+        "percentage": "20",
+        "signatory": null,
+        "accountServing": [],
+        "id": 250
+      },
+      {
+        "name": "new 2",
+        "relationWithCompany": {
+          "name": "Shareholder Individual",
+          "value": "098581"
+        },
+        "belongsTo": {
+          "name": "Mashreq2",
+          "value": "Mashreq2",
+          "level": 2
+        },
+        "percentage": "20",
+        "signatory": null,
+        "accountServing": [],
+        "id": 150
+      }
+    ]
+  };
